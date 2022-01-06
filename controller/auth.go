e83af22c79115dce42d10c80d60dc7b97bada187
Suryansh Kumar <201951158@iiitvadodara.ac.in>
2022-01-06 08:42:25 +0530

    add format and validate function in login and signup

--- controller/auth.go
+++ controller/auth.go
@@ -3,7 +3,9 @@ package controller
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
+	"strings"
 	"vartalap/database"
 	"vartalap/model"
 
@@ -17,16 +19,24 @@ func UserSignUp(w http.ResponseWriter, r *http.Request) {
 	var user model.User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	user, err = formatAndValidateForSignUp(user)
+	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-
+	if alreadyExists(user.Username) {
+		w.WriteHeader(http.StatusAccepted)
+		json.NewEncoder(w).Encode(bson.M{
+			"message": "Username already exists",
+		})
+		return
+	}
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(bson.M{
-			"message": "hash nai bna",
-		})
 		return
 	}
 	user.Password = string(hashedPassword)
@@ -34,9 +44,6 @@ func UserSignUp(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(bson.M{
-			"message": "hash to hua but store nahi hua db mein",
-		})
 		return
 	}
 	json.NewEncoder(w).Encode(bson.M{
@@ -45,46 +52,102 @@ func UserSignUp(w http.ResponseWriter, r *http.Request) {
 }
 
 func UserLogIn(w http.ResponseWriter, r *http.Request) {
-	var user, foundUser model.User
-	json.NewDecoder(r.Body).Decode(&user)
-	filter := bson.M{"username": user.Username}
-	cursor, err := userCollection.Find(context.TODO(), filter)
+	var inputData, foundUser model.User
+	err := json.NewDecoder(r.Body).Decode(&inputData)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	for cursor.Next(context.TODO()) {
-		err = cursor.Decode(&foundUser)
-		if err != nil {
-			w.WriteHeader(http.StatusAccepted)
-			json.NewEncoder(w).Encode(bson.M{
-				"message": "No user Found",
-			})
-			return
-		} else if err := bcrypt.CompareHashAndPassword([]byte(foundUser.Password), []byte(user.Password)); err != nil {
-			w.WriteHeader(http.StatusAccepted)
-			json.NewEncoder(w).Encode(bson.M{
-				"message": "No user Found",
-			})
-			return
-		} else {
-			cursor.Close(context.TODO())
-			w.WriteHeader(http.StatusAccepted)
-			json.NewEncoder(w).Encode(bson.M{
-				"message": "User login Successfully",
-			})
-			return
-		}
+	inputData, err = formatAndValidateForLogIn(inputData)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(bson.M{
+			"message": "Bad input data",
+		})
+		return
+	}
+	filter := bson.M{"username": inputData.Username}
+	if err := userCollection.FindOne(context.TODO(), filter).Decode(&foundUser); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(foundUser.Password), []byte(inputData.Password)); err != nil {
+		w.WriteHeader(http.StatusAccepted)
+		json.NewEncoder(w).Encode(bson.M{
+			"message": "No user Found",
+		})
+		return
 	}
 	w.WriteHeader(http.StatusAccepted)
 	json.NewEncoder(w).Encode(bson.M{
-		"message": "No user Found",
+		"message": "User login Successfully",
 	})
 }
 
 func SayHello(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusAccepted)
 	json.NewEncoder(w).Encode(bson.M{
-		"message": "hello from suru chat api",
+		"message": "hello from Vartalap chat api",
 	})
+}
+
+func formatAndValidateForSignUp(user model.User) (model.User, error) {
+	if len(user.Username) == 0 {
+		return user, errors.New("please provide a username")
+	} else {
+		user.Username = strings.TrimSpace(user.Username)
+	}
+
+	if len(user.Password) == 0 {
+		return user, errors.New("please provide a password")
+	} else {
+		user.Password = strings.TrimSpace(user.Password)
+	}
+
+	if len(user.Email) == 0 {
+		return user, errors.New("please provide a email")
+	} else {
+		user.Email = strings.TrimSpace(user.Email)
+	}
+
+	if len(user.Pubkey) == 0 {
+		return user, errors.New("please provide a pubkey")
+	} else {
+		user.Pubkey = strings.TrimSpace(user.Pubkey)
+	}
+	if len(user.Imagename) > 0 {
+		user.Imagename = strings.TrimSpace(user.Imagename)
+	}
+	if len(user.Imageurl) > 0 {
+		user.Imageurl = strings.TrimSpace(user.Imageurl)
+	}
+	return user, nil
+}
+
+func formatAndValidateForLogIn(user model.User) (model.User, error) {
+	if len(user.Username) == 0 {
+		return user, errors.New("please provide a username")
+	} else {
+		user.Username = strings.TrimSpace(user.Username)
+	}
+	if len(user.Password) == 0 {
+		return user, errors.New("please provide a password")
+	} else {
+		user.Password = strings.TrimSpace(user.Password)
+	}
+
+	if len(user.Pubkey) == 0 {
+		return user, errors.New("please provide a pubkey")
+	} else {
+		user.Pubkey = strings.TrimSpace(user.Pubkey)
+	}
+	return user, nil
+}
+
+func alreadyExists(username string) bool {
+	var tempUser model.User
+	_ = userCollection.FindOne(context.TODO(), bson.M{
+		"username": username,
+	}).Decode(&tempUser)
+	return len(tempUser.Username) > 0
 }
