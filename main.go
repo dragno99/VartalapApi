bb2c6ce2269a33745fb43fbda4ab83fe9d5973ea
dragno99 <201951158@iiitvadodara.ac.in>
2022-08-10 01:39:39 +0530

    update backend

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/dragno99/VartalapApi/router"
+	"github.com/gorilla/handlers"
 	"github.com/joho/godotenv"
 )
 
@@ -18,6 +19,7 @@ func main() {
 	PORT := string(os.Getenv("PORT"))
 
 	// start listening
-	log.Fatal(http.ListenAndServe(PORT, myRouter))
+	// log.Fatal(http.ListenAndServe(PORT, myRouter))
+	log.Fatal(http.ListenAndServe(PORT, handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(myRouter)))
 
 }
