230b3f05108f2c0a6f7be580431b71560995e676
dragno99 <201951158@iiitvadodara.ac.in>
2022-08-10 02:39:56 +0530

    update backend

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/dragno99/VartalapApi/router"
 	"github.com/joho/godotenv"
@@ -14,12 +13,12 @@ func main() {
 	// start the router
 	myRouter := router.InitRouter()
 
-	godotenv.Load(".env")
+	godotenv.Load()
 
-	PORT := string(os.Getenv("PORT"))
+	// PORT := string(os.Getenv("PORT"))
 
 	// start listening
-	log.Fatal(http.ListenAndServe(PORT, myRouter))
+	log.Fatal(http.ListenAndServe("8080", myRouter))
 	// log.Fatal(http.ListenAndServe(PORT, handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(myRouter)))
 
 }
