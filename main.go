1358055d560968eaab67f134ca8308b69b2f61b1
dragno99 <201951158@iiitvadodara.ac.in>
2022-08-10 02:45:14 +0530

    update backend

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	// PORT := string(os.Getenv("PORT"))
 
 	// start listening
-	log.Fatal(http.ListenAndServe(":8080", myRouter))
+	log.Fatal(http.ListenAndServe(":92839", myRouter))
 	// log.Fatal(http.ListenAndServe(PORT, handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(myRouter)))
 
 }
