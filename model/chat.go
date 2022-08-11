3ba91dcd5b277f61bf248ffcd2b62cb660d0ff72
dragno99 <201951158@iiitvadodara.ac.in>
2022-08-12 01:02:09 +0530

    update backend

--- model/chat.go
+++ model/chat.go
@@ -8,7 +8,7 @@ type Chat struct {
 	Id       primitive.ObjectID `bson:"_id,omitempty"`
 	User1    primitive.ObjectID `bson:"user1,omitempty"`
 	User2    primitive.ObjectID `bson:"user2,omitempty"`
-	Messages []Message          `bson:"messages,omitempty"`
+	Messages []Message          `bson:"messages"`
 }
 
 type Message struct {
