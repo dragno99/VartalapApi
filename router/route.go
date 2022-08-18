439d306cbccf67e259bd92d85a149e13af98eaff
dragno99 <201951158@iiitvadodara.ac.in>
2022-08-18 11:19:38 +0530

    update backend

--- router/route.go
+++ router/route.go
@@ -1,8 +1,6 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/dragno99/VartalapApi/controller"
 	"github.com/dragno99/VartalapApi/middleware"
 
@@ -18,7 +16,7 @@ func InitRouter() *mux.Router {
 	router.Handle("/appusers/", middleware.IsAuthorized(controller.GetAppUser)).Methods("GET")
 	router.Handle("/chatmessages/{chatId}", middleware.IsAuthorized(controller.GetChatMessages)).Methods("GET")
 	router.Handle("/startchat/", middleware.IsAuthorized(controller.StartChat)).Methods("POST")
-	router.Handle("/joinChatRoom/{chatId}", http.HandlerFunc(controller.JoinChatRoom))
+	router.Handle("/joinChatRoom/{chatId}", middleware.IsAuthorized(controller.JoinChatRoom))
 	router.Handle("/message/", middleware.IsAuthorized(controller.AddMessage)).Methods("POST")
 	router.Handle("/updatefullname/", middleware.IsAuthorized(controller.UpdateFullName)).Methods("POST")
 	return router
